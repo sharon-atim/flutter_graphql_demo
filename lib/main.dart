4d0b350517040678f6ae14350c12c13d2ab78d28
Sharon Atim <venusatim@gmail.com>
2022-05-21 17:57:42 +0100

    created graphQL client

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+void main() async {
+// Initializes Hive for storing the cached persist data.
+  await initHiveForFlutter();
+// Http link implementation
+  final HttpLink httpLink = HttpLink('https://api.github.com/graphql');
+
+//Header based authentication that adds a getToken authentication callback.
+  final AuthLink authLink = AuthLink(
+    getToken: () async => 'Bearer <YOUR_PERSONAL_ACCESS_TOKEN>',
+//OR
+// getToken; () => Bearer <YOUR_PERSONAL_ACCESS_TOKEN>
+  );
+
+// The authLink is concatenated with httpLink to attach the github access token.
+  final Link link = authLink.concat(httpLink);
+
+  ValueNotifier<GraphQLClient> client = ValueNotifier(GraphQLClient(
+    link: link,
+    cache: GraphQLCache(store: HiveStore()),
+  ));
+}
